8f36dc6d49c72682905338971a353883059d4e6b
codeNoob <1371916369@qq.com>
2024-01-17 15:43:50 +0800

    finish hw03

--- hw/hw03/hw03.py
+++ hw/hw03/hw03.py
@@ -246,10 +246,12 @@ def interval(a, b):
 def lower_bound(x):
     """Return the lower bound of interval x."""
     "*** YOUR CODE HERE ***"
+    return x[0]
 
 def upper_bound(x):
     """Return the upper bound of interval x."""
     "*** YOUR CODE HERE ***"
+    return x[1]
 def str_interval(x):
     """Return a string representation of interval x.
     """
@@ -264,17 +266,20 @@ def add_interval(x, y):
 def mul_interval(x, y):
     """Return the interval that contains the product of any value in x and any
     value in y."""
-    p1 = x[0] * y[0]
-    p2 = x[0] * y[1]
-    p3 = x[1] * y[0]
-    p4 = x[1] * y[1]
-    return [min(p1, p2, p3, p4), max(p1, p2, p3, p4)]
+    p1 = lower_bound(x) * lower_bound(y)
+    p2 = lower_bound(x) * upper_bound(y)
+    p3 = upper_bound(x) * lower_bound(y)
+    p4 = upper_bound(x) * upper_bound(y)
+    return interval(min(p1, p2, p3, p4), max(p1, p2, p3, p4))
 
 
 def sub_interval(x, y):
     """Return the interval that contains the difference between any value in x
     and any value in y."""
     "*** YOUR CODE HERE ***"
+    low = lower_bound(x) - upper_bound(y)
+    upper = upper_bound(x) - lower_bound(y)
+    return interval(low,upper)
 
 
 def div_interval(x, y):
@@ -282,6 +287,7 @@ def div_interval(x, y):
     any value in y. Division is implemented as the multiplication of x by the
     reciprocal of y."""
     "*** YOUR CODE HERE ***"
+    assert lower_bound(y) *upper_bound(y) > 0, 'Divide by zero'
     reciprocal_y = interval(1/upper_bound(y), 1/lower_bound(y))
     return mul_interval(x, reciprocal_y)
 
@@ -303,8 +309,8 @@ def check_par():
     >>> lower_bound(x) != lower_bound(y) or upper_bound(x) != upper_bound(y)
     True
     """
-    r1 = interval(1, 1) # Replace this line!
-    r2 = interval(1, 1) # Replace this line!
+    r1 = interval(1, 3) # Replace this line!
+    r2 = interval(1, 3) # Replace this line!
     return r1, r2
 
 
@@ -322,6 +328,16 @@ def quadratic(x, a, b, c):
     '0 to 10'
     """
     "*** YOUR CODE HERE ***"
+    f = lambda y : a * y ** 2 + b *y + c
+    l = lower_bound(x)
+    r = upper_bound(x)
+    f_l,f_r = f(l),f(r)
+    x0 = -b / (2 * a)
+    if x0 >= l and x0 <= r:
+        f_x0 = f(x0)
+        return interval(min(f_l, f_r, f_x0), max(f_l, f_r, f_x0))
+    else:
+        return interval(min(f_l, f_r), max(f_l, f_r))
 
 
 
