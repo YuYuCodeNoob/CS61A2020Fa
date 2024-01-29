b876e375bc210587f60494f97b091d07a54698b2
codeNoob <1371916369@qq.com>
2024-01-30 03:17:01 +0800

    finish hw07

--- hw/hw07/hw07.scm
+++ hw/hw07/hw07.scm
@@ -1,5 +1,9 @@
 (define (filter-lst fn lst)
-  'YOUR-CODE-HERE
+  (cond
+        ((null? lst) '())
+        ((fn (car lst)) (cons (car lst) (filter-lst fn (cdr lst))))
+        (else (filter-lst fn (cdr lst)))
+  )
 )
 
 ;;; Tests
@@ -10,7 +14,10 @@
 
 
 (define (interleave first second)
-  'YOUR-CODE-HERE
+  (cond ((null? first) second)
+    ((null? second) first)
+    (else (cons (car first) (cons (car second) (interleave (cdr first) (cdr second)))))
+    )
 )
 
 (interleave (list 1 3 5) (list 2 4 6))
@@ -24,11 +31,15 @@
 
 
 (define (accumulate combiner start n term)
-  'YOUR-CODE-HERE
+  (cond ((= 0 n) start )
+    (else (combiner (term n) (accumulate combiner start (- n 1) term))))
 )
 
 
 (define (no-repeats lst)
-  'YOUR-CODE-HERE
+  (cond ((null? lst) '())
+    (else
+          (cons (car lst) (no-repeats (filter (lambda (x) (not (= x (car lst)))) (cdr lst)))))
+  )
 )
 
