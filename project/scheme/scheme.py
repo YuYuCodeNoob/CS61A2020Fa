166b494e44242e8f6cff892cb99dca71f24fbbac
codeNoob <1371916369@qq.com>
2024-02-10 04:39:04 +0800

    finish problem8

--- project/scheme/scheme.py
+++ project/scheme/scheme.py
@@ -312,6 +312,7 @@ def do_lambda_form(expressions, env):
     formals = expressions.first
     validate_formals(formals)
     # BEGIN PROBLEM 8
+    return LambdaProcedure(formals,expressions.rest,env)
     "*** YOUR CODE HERE ***"
     # END PROBLEM 8
 
