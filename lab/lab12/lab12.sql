a3018a162a6cc09600fa039f0fe21c92ba1695cd
codeNoob <1371916369@qq.com>
2024-02-13 02:06:33 +0800

    lab012

--- lab/lab12/lab12.sql
+++ lab/lab12/lab12.sql
@@ -9,13 +9,14 @@ CREATE TABLE bluedog_songs AS
 
 
 CREATE TABLE smallest_int AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT time,smallest FROM students where smallest > 2 ORDER BY smallest LIMIT 20;
 
 
 CREATE TABLE matchmaker AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT a.pet,a.song,a.color,b.color FROM students as a,students as b WHERE a.pet = b.pet AND a.song = b.song 
+  AND a.time < b.time;
 
 
 CREATE TABLE sevens AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT a.seven FROM students as a, numbers as b WHERE a.number = 7 and b.'7' = 'True' and a.time = b.time;
 
